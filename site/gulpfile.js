7d3bff7605c3f4e5942d7681e655f056caa46cc5
armproj <parthi.geo@gmail.com>
2019-09-12 12:42:27 -0400

    gulp change

--- site/gulpfile.js
+++ site/gulpfile.js
@@ -55,12 +55,12 @@ const DEFAULT_CATEGORY = 'Default';
 // BASE_URL is the canonical base URL where the site will reside. This should
 // always include the protocol (http:// or https://) and NOT including a
 // trailing slash.
-const BASE_URL = args.baseUrl || 'https://armorycodelabs.netlify.com';
+const BASE_URL = args.baseUrl || 'https://armorycodelabs.com';
 
 // CODELABS_DIR is the directory where the actual codelabs exist on disk.
 // Despite being a constant, this can be overridden with the --codelabs-dir
 // flag.
-const CODELABS_DIR = args.codelabsDir || '.';
+const CODELABS_DIR = args.codelabsDir || './codelabs';
 
 // CODELABS_ENVIRONMENT is the environment for which to build codelabs.
 const CODELABS_ENVIRONMENT = args.codelabsEnv || 'web';
